548b237dff2e7ddd0cb0a29b42035d2ab96f7159
Chris Lush <chris.lush@gymshark.com>
2021-03-08 13:39:00 +0000

    Implement a Redis cache wrapper for a datastore reader

--- go.mod
+++ go.mod
@@ -4,10 +4,15 @@ go 1.16
 
 require (
 	github.com/georgysavva/scany v0.2.7
+	github.com/go-redis/cache/v8 v8.3.1
+	github.com/go-redis/redis/v8 v8.4.4
 	github.com/golang/protobuf v1.4.3
 	github.com/hashicorp/go-retryablehttp v0.6.8
 	github.com/jackc/pgx/v4 v4.10.1
 	github.com/sirupsen/logrus v1.8.0
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20210112080510-489259a85091 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/grpc v1.36.0
 	google.golang.org/protobuf v1.25.0
 )
---
+++ internal/api/cache.go
@@ -0,0 +1,74 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/go-redis/cache/v8"
+	"github.com/go-redis/redis/v8"
+
+	"github.com/lushc/hacker-news-scraper/internal/datastore"
+)
+
+const (
+	redisHostEnv = "SERVER_PORT"
+	ttl          = 5 * time.Minute
+)
+
+var (
+	errRedisHostEnv = fmt.Errorf("missing env var %s", redisHostEnv)
+)
+
+type CachedReader struct {
+	cache  *cache.Cache
+	reader datastore.Reader
+}
+
+func NewCachedReader(reader datastore.Reader) (*CachedReader, error) {
+	// TODO: viper config instead
+	host, ok := os.LookupEnv("REDIS_HOST")
+	if !ok {
+		return nil, errRedisHostEnv
+	}
+
+	redisCache := cache.New(&cache.Options{
+		Redis: redis.NewClient(&redis.Options{
+			Addr: host,
+		}),
+	})
+
+	return &CachedReader{
+		cache:  redisCache,
+		reader: reader,
+	}, nil
+}
+
+func (c CachedReader) All(ctx context.Context) (items []*datastore.Item, err error) {
+	err = c.cache.Once(&cache.Item{
+		Key:   "all-stories",
+		Value: &items,
+		TTL:   ttl,
+		Do: func(item *cache.Item) (interface{}, error) {
+			return c.reader.All(ctx)
+		},
+	})
+	return items, err
+}
+
+func (c CachedReader) ByItemType(ctx context.Context, itemType datastore.ItemType) (items []*datastore.Item, err error) {
+	err = c.cache.Once(&cache.Item{
+		Key:   fmt.Sprintf("%s-stories", itemType),
+		Value: &items,
+		TTL:   ttl,
+		Do: func(item *cache.Item) (interface{}, error) {
+			return c.reader.ByItemType(ctx, itemType)
+		},
+	})
+	return items, err
+}
+
+func (c CachedReader) Close() {
+	c.reader.Close()
+}
